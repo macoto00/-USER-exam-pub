cf68399571bd0bee049606357c62d3315d0aa191
TomasMacoun <macount@seznam.cz>
2023-10-08 15:56:10 +0200

    Update DrinkServiceImpl.java

--- etykaDigital/src/main/java/services/DrinkServiceImpl.java
+++ etykaDigital/src/main/java/services/DrinkServiceImpl.java
@@ -4,8 +4,10 @@ import DTOs.SummaryDTO;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import models.Drink;
+import models.Order;
 import org.springframework.stereotype.Service;
 import repositories.DrinkRepository;
+import repositories.OrderRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,8 @@ import java.util.Optional;
 public class DrinkServiceImpl implements DrinkService {
 
     private DrinkRepository drinkRepository;
+    private OrderRepository orderRepository;
+    private OrderService orderService;
 
     @Override
     public List<Drink> getDrinkMenu() {
@@ -23,11 +27,47 @@ public class DrinkServiceImpl implements DrinkService {
     }
 
     @Override
-    public List<SummaryDTO> getDrinkSummaries() {
+    public List<SummaryDTO> getAllDrinkSummaries() {
         List<Drink> drinks = drinkRepository.findAll();
 
         List<SummaryDTO> summaryList = new ArrayList<>();
         for (Drink drink : drinks) {
+            SummaryDTO summaryDTO = createSummaryDTO(drink);
+            summaryList.add(summaryDTO);
+        }
+
+        return summaryList;
+    }
+
+    @Override
+    public List<SummaryDTO> getDrinkSummariesByProduct() {
+        List<Drink> drinks = drinkRepository.findAll();
+        List<SummaryDTO> summaryList = new ArrayList<>();
+
+        for (Drink drink : drinks) {
+            List<Order> orders = orderService.getOrdersByDrinkId(drink.getId());
+
+            for (Order order : orders) {
+                SummaryDTO summaryDTO = new SummaryDTO();
+                summaryDTO.setProductName(drink.getProductName());
+                summaryDTO.setTotalAmount(order.getAmount());
+                summaryDTO.setUnitPrice(order.getPrice());
+                summaryDTO.setSummaryPrice(order.getAmount() * order.getPrice());
+
+                summaryList.add(summaryDTO);
+            }
+        }
+
+        return summaryList;
+    }
+
+    @Override
+    public List<SummaryDTO> getDrinkSummariesByUser(Long userId) {
+        List<SummaryDTO> summaryList = new ArrayList<>();
+        List<Order> userOrders = orderRepository.findByUserId(userId);
+
+        for (Order order : userOrders) {
+            Drink drink = order.getDrink();
             SummaryDTO summaryDTO = createSummaryDTO(drink);
             summaryList.add(summaryDTO);
         }
