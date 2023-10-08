51d2f59921305201b7d336be8f95710dca26dda7
TomasMacoun <macount@seznam.cz>
2023-10-08 15:32:29 +0200

    Update DrinkService.java

--- etykaDigital/src/main/java/services/DrinkService.java
+++ etykaDigital/src/main/java/services/DrinkService.java
@@ -1,9 +1,12 @@
 package services;
 
+import DTOs.SummaryDTO;
 import models.Drink;
 
 import java.util.List;
 
 public interface DrinkService {
     List<Drink> getDrinkMenu();
+    List<SummaryDTO> getDrinkSummaries();
+    void updateDrink(Long id, int amount);
 }
