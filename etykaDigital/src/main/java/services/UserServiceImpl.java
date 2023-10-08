a94415eed9e17d6b7548d6629c0d333bdd0f55de
TomasMacoun <macount@seznam.cz>
2023-10-08 13:58:31 +0200

    Update UserServiceImpl.java

--- etykaDigital/src/main/java/services/UserServiceImpl.java
+++ etykaDigital/src/main/java/services/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package services;
 
+import DTOs.CreateUserDTO;
 import DTOs.UpdateUserDTO;
+import exceptions.EmailAlreadyInUseException;
 import lombok.AllArgsConstructor;
 import models.User;
+import models.UserUpdateResponse;
 import org.springframework.stereotype.Service;
 import repositories.UserRepository;
 
@@ -26,26 +29,49 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User createUser(User user) {
+    public User createUser(CreateUserDTO createUserDTO) {
+        if (userRepository.findByEmail(createUserDTO.getEmail()).isPresent()) {
+            throw new EmailAlreadyInUseException("Email is already in use. Please choose a different email.");
+        }
+
+        User user = new User();
+        user.setName(createUserDTO.getName());
+        user.setActive(createUserDTO.isActive());
+        user.setAdult(createUserDTO.isAdult());
+        user.setPocket(createUserDTO.getPocket());
+        user.setRole(createUserDTO.getRole());
+        user.setEmail(createUserDTO.getEmail());
+        user.setPassword(createUserDTO.getPassword());
         userRepository.save(user);
         return user;
     }
 
     @Override
-    public User updateUser(Long userId, UpdateUserDTO updateUserDTO) {
+    public UserUpdateResponse updateUser(Long userId, UpdateUserDTO updateUserDTO) {
         Optional<User> optionalUser = userRepository.findById(userId);
 
         if (optionalUser.isPresent()) {
             User existingUser = optionalUser.get();
+
+            if (!updateUserDTO.getEmail().equals(existingUser.getEmail())
+                    && userRepository.findByEmail(updateUserDTO.getEmail()).isPresent()) {
+                return new UserUpdateResponse(null, "Email is already in use by another user.");
+            }
+
             existingUser.setName(updateUserDTO.getName());
             existingUser.setActive(updateUserDTO.isActive());
             existingUser.setAdult(updateUserDTO.isAdult());
             existingUser.setPocket(updateUserDTO.getPocket());
+            existingUser.setEmail(updateUserDTO.getEmail());
+            existingUser.setPassword(updateUserDTO.getPassword());
 
             userRepository.save(existingUser);
+            return new UserUpdateResponse(existingUser, "User updated successfully.");
         }
-        return null;
+
+        return new UserUpdateResponse(null, "User not found with ID: " + userId);
     }
+
 
     @Override
     public void deleteUser(Long userId) {
