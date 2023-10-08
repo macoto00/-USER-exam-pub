25fad62f5f3e98c6ed41b3b8258329ff6c37ad00
TomasMacoun <macount@seznam.cz>
2023-10-08 15:56:05 +0200

    Update OrderRepository.java

--- etykaDigital/src/main/java/repositories/OrderRepository.java
+++ etykaDigital/src/main/java/repositories/OrderRepository.java
@@ -4,6 +4,10 @@ import models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByDrinkId(Long drinkId);
+    List<Order> findByUserId(Long userId);
 }
