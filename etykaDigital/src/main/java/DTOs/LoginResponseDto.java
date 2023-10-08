cfc63b5e9cbb1d9885ba97b00a0106276735dc78
TomasMacoun <macount@seznam.cz>
2023-10-08 18:34:34 +0200

    Create LoginResponseDto.java

---
+++ etykaDigital/src/main/java/DTOs/LoginResponseDto.java
@@ -0,0 +1,12 @@
+package DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDto {
+    private String email;
+}
