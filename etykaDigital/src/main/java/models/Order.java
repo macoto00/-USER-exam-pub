1e93b012b0d206ef5355e14df4a56a0ecd1dffb8
TomasMacoun <macount@seznam.cz>
2023-10-08 11:18:12 +0200

    created Order model class

---
+++ etykaDigital/src/main/java/models/Order.java
@@ -0,0 +1,28 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String productName;
+    private int amount;
+    private double price;
+    @ManyToOne
+    private User user;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
