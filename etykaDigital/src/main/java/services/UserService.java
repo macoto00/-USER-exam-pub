01389e67d8c2504c4c340de671e251fc57ee3b4a
TomasMacoun <macount@seznam.cz>
2023-10-08 11:48:38 +0200

    Update UserService.java

--- etykaDigital/src/main/java/services/UserService.java
+++ etykaDigital/src/main/java/services/UserService.java
@@ -1,4 +1,15 @@
 package services;
 
+import DTOs.UpdateUserDTO;
+import models.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
+    List<User> getAllUsers();
+    Optional<User> getUserById(Long userId);
+    void createUser(User user);
+    void updateUser(Long userId, UpdateUserDTO updateUserDTO);
+    void deleteUser(Long userId);
 }
