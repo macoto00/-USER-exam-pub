45fa53b957c7df07ae3f62455992483980229317
TomasMacoun <macount@seznam.cz>
2023-10-08 13:58:23 +0200

    Create UserUpdateResponse.java

---
+++ etykaDigital/src/main/java/models/UserUpdateResponse.java
@@ -0,0 +1,13 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateResponse {
+    private User user;
+    private String message;
+}
