b1a7604a209776247eb9ed8b85bc8721064912d4
TomasMacoun <macount@seznam.cz>
2023-10-08 15:56:12 +0200

    Update OrderService.java

--- etykaDigital/src/main/java/services/OrderService.java
+++ etykaDigital/src/main/java/services/OrderService.java
@@ -1,5 +1,10 @@
 package services;
 
+import models.Order;
+
+import java.util.List;
+
 public interface OrderService {
     public boolean processPurchase(Long userId, Long productId, double productPrice, int amount);
+    List<Order> getOrdersByDrinkId(Long drinkId);
 }
