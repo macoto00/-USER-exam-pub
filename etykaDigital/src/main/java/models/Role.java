ec66d6fa7f8792c3e62c8cbe859b2499bddb216d
TomasMacoun <macount@seznam.cz>
2023-10-08 18:35:01 +0200

    Create Role.java

---
+++ etykaDigital/src/main/java/models/Role.java
@@ -0,0 +1,33 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public static List<Role> getRoleList() {
+        return List.of(
+                new Role("USER"),
+                new Role("ADMIN")
+        );
+    }
+}
