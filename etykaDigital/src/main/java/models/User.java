6ddb7c32989e46611bc408b155b66e7474370e51
TomasMacoun <macount@seznam.cz>
2023-10-08 11:48:34 +0200

    Update User.java

--- etykaDigital/src/main/java/models/User.java
+++ etykaDigital/src/main/java/models/User.java
@@ -5,11 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
