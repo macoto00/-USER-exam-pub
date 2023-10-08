925ff62a4f83a13ff4d6ddfb3e6eba5479055ed6
TomasMacoun <macount@seznam.cz>
2023-10-08 14:49:04 +0200

    Update DrinkServiceImpl.java

--- etykaDigital/src/main/java/services/DrinkServiceImpl.java
+++ etykaDigital/src/main/java/services/DrinkServiceImpl.java
@@ -1,7 +1,20 @@
 package services;
 
+import lombok.AllArgsConstructor;
+import models.Drink;
 import org.springframework.stereotype.Service;
+import repositories.DrinkRepository;
+
+import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class DrinkServiceImpl implements DrinkService {
+
+    private DrinkRepository drinkRepository;
+
+    @Override
+    public List<Drink> getDrinkMenu() {
+        return drinkRepository.findAll();
+    }
 }
