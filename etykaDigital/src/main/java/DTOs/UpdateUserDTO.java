3eed7411066ccff40f562b10834b62694fb92c62
TomasMacoun <macount@seznam.cz>
2023-10-08 13:58:16 +0200

    Update UpdateUserDTO.java

--- etykaDigital/src/main/java/DTOs/UpdateUserDTO.java
+++ etykaDigital/src/main/java/DTOs/UpdateUserDTO.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class UpdateUserDTO {
+    private String email;
+    private String password;
     private String name;
     private boolean isActive;
     private boolean isAdult;
