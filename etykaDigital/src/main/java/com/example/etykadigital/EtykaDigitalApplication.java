c5ad23b3c086b69938cc1b766a919774f0135252
TomasMacoun <macount@seznam.cz>
2023-10-08 10:50:01 +0200

    initial commit

---
+++ etykaDigital/src/main/java/com/example/etykadigital/EtykaDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.etykadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtykaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtykaDigitalApplication.class, args);
+	}
+
+}
---
+++ etykaDigital/src/test/java/com/example/etykadigital/EtykaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.etykadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtykaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
