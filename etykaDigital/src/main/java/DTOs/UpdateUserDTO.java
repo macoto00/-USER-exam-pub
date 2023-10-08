c585f562ce6f6b91d14ea66dddb1ccb97f80cf53
TomasMacoun <macount@seznam.cz>
2023-10-08 11:48:22 +0200

    Create UpdateUserDTO.java

---
+++ etykaDigital/src/main/java/DTOs/UpdateUserDTO.java
@@ -0,0 +1,15 @@
+package DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UpdateUserDTO {
+    private String name;
+    private boolean isActive;
+    private boolean isAdult;
+    private double pocket;
+}
